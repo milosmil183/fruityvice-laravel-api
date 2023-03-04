a8ce5ff3d7e122a007b0cad31885356f444b0444
milosmil183 <milosmilovanovic.wk@gmail.com>
2023-03-04 10:37:27 +0100

    Added `Fruit` model & database migration

---
+++ app/Models/Fruit.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fruit extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'fruit_id',
+        'name',
+        'family',
+        'genus',
+        'order',
+        'nutritions',
+        'carbohydrates',
+        'protein',
+        'fat',
+        'calories',
+        'sugar',
+    ];
+}
---
+++ database/migrations/2023_03_04_093125_create_fruits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->id();
+            $table->string('fruit_id');
+            $table->string('name');
+            $table->string('family');
+            $table->string('genus');
+            $table->string('order');
+            $table->string('nutritions');
+            $table->integer('carbohydrates');
+            $table->integer('protein');
+            $table->integer('fat');
+            $table->integer('calories');
+            $table->integer('sugar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fruits');
+    }
+};
